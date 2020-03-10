a11297a5c09dbbfbc81a96b51e431daddb60ed53
Henrik Lindberg <563066+hlindberg@users.noreply.github.com>
2020-03-10 20:09:03 +0100

    Make pub command accept topic and msg
    Before this, the pub command send hardcoded
    message on hardcoded topic. These can now
    be given on the command line with --topic/-t and --message/-m.

--- cmd/pub.go
+++ cmd/pub.go
@@ -13,7 +13,7 @@ var publishCmd = &cobra.Command{
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Call the busn logic
-		mqtt.Publish(MQQTBroker, "dummytopic", "dummy message")
+		mqtt.Publish(MQQTBroker, Topic, Message)
 	},
 
 	Args: func(cmd *cobra.Command, args []string) error {
@@ -25,9 +25,17 @@ var publishCmd = &cobra.Command{
 // MQQTBroker is the MQTT host:port to dial
 var MQQTBroker string
 
+// Topic is the MQTT topic to publish to
+var Topic string
+
+// Message is the MQTT message text to publish
+var Message string
+
 func init() {
 	RootCmd.AddCommand(publishCmd)
 	flags := publishCmd.PersistentFlags()
 
-	flags.StringVarP(&MQQTBroker, "broker", "b", "mqtt.eclipse.org", "the MQTT Broker host to connect to (IP or name)")
+	flags.StringVarP(&MQQTBroker, "broker", "b", "mqtt.eclipse.org", "the MQTT Broker host to connect to (default 'mgtt.eclipse.org')")
+	flags.StringVarP(&Topic, "topic", "t", "test", "the MQTT topic to send message to (default 'test')")
+	flags.StringVarP(&Message, "message", "m", "", "the message to send")
 }
